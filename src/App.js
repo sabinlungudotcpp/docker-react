578ae5bc41f2158aadebbbe0eefe9ba779122254
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-02-01 18:28:12 +0000

    Added access and secret key for deloying over to AWS

--- src/App.js
+++ src/App.js
@@ -6,17 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Learn React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Change
-        </a>
+        <p>Learn React</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Change</a>
       </header>
     </div>
   );
